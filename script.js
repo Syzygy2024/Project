58241fb34f09b402ce01f938a653ee20d3d74c70
Zotel Mabiria <160586177+Syzygy2024@users.noreply.github.com>
2024-06-17 16:56:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('registerForm').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    const response = await fetch('http://localhost:5000/api/users/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, email, password })
+    });
+    
+    const result = await response.json();
+    document.getElementById('response').innerText = result.message;
+  });
+  
